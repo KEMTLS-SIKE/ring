b05cebd30480daf2e17c0610a9b549cc551f5765
Thom Wiggers <thom@thomwiggers.nl>
2019-05-27 09:46:32 +0200

    more sphincses

--- src/pqsign.rs
+++ src/pqsign.rs
@@ -6,17 +6,49 @@
 //!     * Only RSA has PKCS1
 //! * Import signature schemes from pqcrypto
 
-use pqcrypto::{
-    sign::sphincsshake256128fsimple,
-    traits::sign::{DetachedSignature, PublicKey, SecretKey},
-};
+use pqcrypto::prelude::*;
 
 use crate::{error, io::der, pkcs8, sealed, signature};
 use untrusted;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum AlgorithmID {
-    SPHINCS_SHAKE_256_128F_SIMPLE = 0xFE01,
+    SPHINCS_SHA_256_128S_SIMPLE = 0xFE00,
+    SPHINCS_SHA_256_128S_ROBUST = 0xFE01,
+    SPHINCS_SHA_256_128F_SIMPLE = 0xFE02,
+    SPHINCS_SHA_256_128F_ROBUST = 0xFE03,
+    SPHINCS_SHA_256_192S_SIMPLE = 0xFE04,
+    SPHINCS_SHA_256_192S_ROBUST = 0xFE05,
+    SPHINCS_SHA_256_192F_SIMPLE = 0xFE06,
+    SPHINCS_SHA_256_192F_ROBUST = 0xFE07,
+    SPHINCS_SHA_256_256S_SIMPLE = 0xFE08,
+    SPHINCS_SHA_256_256S_ROBUST = 0xFE09,
+    SPHINCS_SHA_256_256F_SIMPLE = 0xFE0A,
+    SPHINCS_SHA_256_256F_ROBUST = 0xFE0B,
+    SPHINCS_SHAKE_256_128S_SIMPLE = 0xFE0C,
+    SPHINCS_SHAKE_256_128S_ROBUST = 0xFE0D,
+    SPHINCS_SHAKE_256_128F_SIMPLE = 0xFE0E,
+    SPHINCS_SHAKE_256_128F_ROBUST = 0xFE0F,
+    SPHINCS_SHAKE_256_192S_SIMPLE = 0xFE10,
+    SPHINCS_SHAKE_256_192S_ROBUST = 0xFE11,
+    SPHINCS_SHAKE_256_192F_SIMPLE = 0xFE12,
+    SPHINCS_SHAKE_256_192F_ROBUST = 0xFE13,
+    SPHINCS_SHAKE_256_256S_SIMPLE = 0xFE14,
+    SPHINCS_SHAKE_256_256S_ROBUST = 0xFE15,
+    SPHINCS_SHAKE_256_256F_SIMPLE = 0xFE16,
+    SPHINCS_SHAKE_256_256F_ROBUST = 0xFE17,
+    SPHINCS_HARAKA_128S_SIMPLE = 0xFE18,
+    SPHINCS_HARAKA_128S_ROBUST = 0xFE19,
+    SPHINCS_HARAKA_128F_SIMPLE = 0xFE1A,
+    SPHINCS_HARAKA_128F_ROBUST = 0xFE1B,
+    SPHINCS_HARAKA_192S_SIMPLE = 0xFE1C,
+    SPHINCS_HARAKA_192S_ROBUST = 0xFE1D,
+    SPHINCS_HARAKA_192F_SIMPLE = 0xFE1E,
+    SPHINCS_HARAKA_192F_ROBUST = 0xFE1F,
+    SPHINCS_HARAKA_256S_SIMPLE = 0xFE20,
+    SPHINCS_HARAKA_256S_ROBUST = 0xFE21,
+    SPHINCS_HARAKA_256F_SIMPLE = 0xFE22,
+    SPHINCS_HARAKA_256F_ROBUST = 0xFE23,
 }
 
 // old stuff below
@@ -110,7 +142,11 @@ pub struct PQSignatureScheme {
 
     pub id: AlgorithmID,
 
-    pub verify: for<'a, 'b, 'c> fn(&'a [u8], &'b PQSignature, &'c PQPublicKey) -> bool,
+    pub verify: for<'a, 'b, 'c> fn(
+        &'a [u8],
+        &'b PQSignature,
+        &'c PQPublicKey,
+    ) -> Result<(), Box<dyn std::error::Error>>,
 
     pub pk_from_slice: fn(&[u8]) -> PQPublicKey,
     pub sk_from_slice: fn(&[u8]) -> PQSecretKey,
@@ -129,7 +165,7 @@ impl signature::VerificationAlgorithm for PQSignatureScheme {
             id: self.id,
             signature: signature.as_slice_less_safe().to_vec(),
         };
-        if (self.verify)(msg.as_slice_less_safe(), &sig, &pk) {
+        if let Ok(()) = (self.verify)(msg.as_slice_less_safe(), &sig, &pk) {
             Ok(())
         } else {
             Err(error::Unspecified)
@@ -137,46 +173,88 @@ impl signature::VerificationAlgorithm for PQSignatureScheme {
     }
 }
 
-#[allow(unused)]
-pub static SPHINCS_SHAKE_256_128F_SIMPLE: PQSignatureScheme = PQSignatureScheme {
-    id: AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE,
-    keypair: || {
-        let (pk, sk) = sphincsshake256128fsimple::keypair();
-        let pqpk = PQPublicKey {
-            alg: &SPHINCS_SHAKE_256_128F_SIMPLE,
-            key: pk.as_bytes().to_vec(),
+macro_rules! sphincs_scheme {
+    ($id: ident, $ns: ident) => {
+        use pqcrypto::sign::$ns;
+        pub static $id: PQSignatureScheme = PQSignatureScheme {
+            id: AlgorithmID::$id,
+            keypair: || {
+                let (pk, sk) = $ns::keypair();
+                let pqpk = PQPublicKey {
+                    alg: &$id,
+                    key: pk.as_bytes().to_vec(),
+                };
+                let pqsk = PQSecretKey {
+                    alg: &$id,
+                    key: sk.as_bytes().to_vec(),
+                };
+                PQKeyPair { pk: pqpk, sk: pqsk }
+            },
+            pk_from_slice: |pk: &[u8]| PQPublicKey {
+                alg: &$id,
+                key: pk.to_vec(),
+            },
+            sk_from_slice: |sk: &[u8]| PQSecretKey {
+                alg: &$id,
+                key: sk.to_vec(),
+            },
+            sign: |message: &[u8], sk: &PQSecretKey| {
+                debug_assert_eq!(sk.alg.id, AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE);
+                let sk = $ns::SecretKey::from_bytes(&sk.key).unwrap();
+                let sig = $ns::detached_sign(message, &sk);
+                PQSignature {
+                    id: AlgorithmID::$id,
+                    signature: sig.as_bytes().to_vec(),
+                }
+            },
+            verify: |message: &[u8], sig: &PQSignature, pk: &PQPublicKey| {
+                debug_assert_eq!(pk.alg.id, AlgorithmID::$id);
+                debug_assert_eq!(sig.id, AlgorithmID::$id);
+                let sig = $ns::DetachedSignature::from_bytes(&sig.signature)?;
+                let pk = $ns::PublicKey::from_bytes(&pk.key)?;
+                $ns::verify_detached_signature(&sig, message, &pk)?;
+                Ok(())
+            },
         };
-        let pqsk = PQSecretKey {
-            alg: &SPHINCS_SHAKE_256_128F_SIMPLE,
-            key: sk.as_bytes().to_vec(),
-        };
-        PQKeyPair { pk: pqpk, sk: pqsk }
-    },
-    pk_from_slice: |pk: &[u8]| PQPublicKey {
-        alg: &SPHINCS_SHAKE_256_128F_SIMPLE,
-        key: pk.to_vec(),
-    },
-    sk_from_slice: |sk: &[u8]| PQSecretKey {
-        alg: &SPHINCS_SHAKE_256_128F_SIMPLE,
-        key: sk.to_vec(),
-    },
-    sign: |message: &[u8], sk: &PQSecretKey| {
-        debug_assert_eq!(sk.alg.id, AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE);
-        let sk = sphincsshake256128fsimple::SecretKey::from_bytes(&sk.key);
-        let sig = sphincsshake256128fsimple::detached_sign(message, &sk);
-        PQSignature {
-            id: AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE,
-            signature: sig.as_bytes().to_vec(),
-        }
-    },
-    verify: |message: &[u8], sig: &PQSignature, pk: &PQPublicKey| {
-        debug_assert_eq!(pk.alg.id, AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE);
-        debug_assert_eq!(sig.id, AlgorithmID::SPHINCS_SHAKE_256_128F_SIMPLE);
-        let sig = sphincsshake256128fsimple::DetachedSignature::from_bytes(&sig.signature);
-        let pk = sphincsshake256128fsimple::PublicKey::from_bytes(&pk.key);
-        sphincsshake256128fsimple::verify_detached_signature(&sig, message, &pk).is_ok()
-    },
-};
+    };
+}
+
+sphincs_scheme!(SPHINCS_SHA_256_128S_SIMPLE, sphincssha256128ssimple);
+sphincs_scheme!(SPHINCS_SHA_256_128S_ROBUST, sphincssha256128srobust);
+sphincs_scheme!(SPHINCS_SHA_256_128F_SIMPLE, sphincssha256128fsimple);
+sphincs_scheme!(SPHINCS_SHA_256_128F_ROBUST, sphincssha256128frobust);
+sphincs_scheme!(SPHINCS_SHA_256_192S_SIMPLE, sphincssha256192ssimple);
+sphincs_scheme!(SPHINCS_SHA_256_192S_ROBUST, sphincssha256192srobust);
+sphincs_scheme!(SPHINCS_SHA_256_192F_SIMPLE, sphincssha256192fsimple);
+sphincs_scheme!(SPHINCS_SHA_256_192F_ROBUST, sphincssha256192frobust);
+sphincs_scheme!(SPHINCS_SHA_256_256S_SIMPLE, sphincssha256256ssimple);
+sphincs_scheme!(SPHINCS_SHA_256_256S_ROBUST, sphincssha256256srobust);
+sphincs_scheme!(SPHINCS_SHA_256_256F_SIMPLE, sphincssha256256fsimple);
+sphincs_scheme!(SPHINCS_SHA_256_256F_ROBUST, sphincssha256256frobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_128S_SIMPLE, sphincsshake256128ssimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_128S_ROBUST, sphincsshake256128srobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_128F_SIMPLE, sphincsshake256128fsimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_128F_ROBUST, sphincsshake256128frobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_192S_SIMPLE, sphincsshake256192ssimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_192S_ROBUST, sphincsshake256192srobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_192F_SIMPLE, sphincsshake256192fsimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_192F_ROBUST, sphincsshake256192frobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_256S_SIMPLE, sphincsshake256256ssimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_256S_ROBUST, sphincsshake256256srobust);
+sphincs_scheme!(SPHINCS_SHAKE_256_256F_SIMPLE, sphincsshake256256fsimple);
+sphincs_scheme!(SPHINCS_SHAKE_256_256F_ROBUST, sphincsshake256256frobust);
+sphincs_scheme!(SPHINCS_HARAKA_128S_SIMPLE, sphincsharaka128ssimple);
+sphincs_scheme!(SPHINCS_HARAKA_128S_ROBUST, sphincsharaka128srobust);
+sphincs_scheme!(SPHINCS_HARAKA_128F_SIMPLE, sphincsharaka128fsimple);
+sphincs_scheme!(SPHINCS_HARAKA_128F_ROBUST, sphincsharaka128frobust);
+sphincs_scheme!(SPHINCS_HARAKA_192S_SIMPLE, sphincsharaka192ssimple);
+sphincs_scheme!(SPHINCS_HARAKA_192S_ROBUST, sphincsharaka192srobust);
+sphincs_scheme!(SPHINCS_HARAKA_192F_SIMPLE, sphincsharaka192fsimple);
+sphincs_scheme!(SPHINCS_HARAKA_192F_ROBUST, sphincsharaka192frobust);
+sphincs_scheme!(SPHINCS_HARAKA_256S_SIMPLE, sphincsharaka256ssimple);
+sphincs_scheme!(SPHINCS_HARAKA_256S_ROBUST, sphincsharaka256srobust);
+sphincs_scheme!(SPHINCS_HARAKA_256F_SIMPLE, sphincsharaka256fsimple);
+sphincs_scheme!(SPHINCS_HARAKA_256F_ROBUST, sphincsharaka256frobust);
 
 #[cfg(test)]
 mod tests {
@@ -192,9 +270,9 @@ mod tests {
         let mut message = [0u8; 64];
         let keypair = (scheme.keypair)();
         let sig = (scheme.sign)(&message, &keypair.sk);
-        assert!((scheme.verify)(&message, &sig, &keypair.pk));
+        assert!((scheme.verify)(&message, &sig, &keypair.pk).is_ok());
         message[10] = 1;
-        assert!(!(scheme.verify)(&message, &sig, &keypair.pk));
+        assert!(!(scheme.verify)(&message, &sig, &keypair.pk).is_ok());
     }
 
     #[test]
